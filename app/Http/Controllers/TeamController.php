4718702f7b701c4338d7c97d890cf7bc03c9ae8b
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-10-13 11:15:28 +0300

    back

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -10,7 +10,7 @@ class TeamController extends Controller
 {
         public function index()
 {
-    $team = Team::orderBy('created_at', 'asc')->get();
+    $team = Team::orderBy('created_at', 'desc')->get();
 
     return response()->json([
         'status' => 'success',
